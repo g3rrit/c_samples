3527f0a130701066a1db7167bc9c1aacc0c42d7a
pearisgreen <grproessl@web.de>
2018-01-27 23:01:45 +0100

    updated container

--- src/container/container.c
+++ src/container/container.c
@@ -41,6 +41,9 @@ void *list_remove_at(struct list *this, int x);
 struct map_node
 {
     struct map_node *next;
+#ifndef S_LINKED
+    struct map_node *prev;
+#endif
     char *key;
     void *data;
 };
@@ -60,8 +63,8 @@ void map_delete_all(struct map *this);
 void map_push_front(struct map *this, char *key, void *data);
 void map_push_back(struct map *this, char *key, void *data);
 
-void map_pop_front(struct map *this, char *key, void *data);
-void map_pop_back(struct map *this, char *key, void *data);
+void *map_pop_front(struct map *this);
+void *map_pop_back(struct map *this);
 
 void *map_get(struct map *this, char *key);
 void *map_at(struct map *this, int x);
@@ -350,62 +353,323 @@ void map_init(struct map *this)
 
 void map_delete(struct map *this)
 {
+    if(!this->head)
+        return;
+
+    struct map_node *entry = this->head;
+    struct map_node *free_entry;
+
+    while(entry->next)
+    {
+        free_entry = entry;
+        entry = entry->next;
+        free(free_entry->key);
+        free(free_entry);
+    }
+    free(entry->key);
+    free(entry);
+
+    this->head = 0;
+    this->tail = 0;
+    this->size = 0;
 }
 
 void map_delete_all(struct map *this)
 {
+    if(!this->head)
+        return;
+
+    struct map_node *entry = this->head;
+    struct map_node *free_entry;
+
+    while(entry->next)
+    {
+        if(!entry->data)
+            free(entry->data);
+
+        free_entry = entry;
+        entry = entry->next;
+        free(free_entry->key);
+        free(free_entry);
+    }
+    if(!entry->data)
+        free(entry->data);
+    free(entry->key);
+    free(entry);
+
+    this->head = 0;
+    this->tail = 0;
+    this->size = 0;
 }
+
+//note: key get saved in map_node without 0 terminator
 
 void map_push_front(struct map *this, char *key, void *data)
 {
+    struct map_node *entry = malloc(sizeof(struct map_node));
+
+    entry->data = data;
+    entry->key = malloc(sizeof(char) * strlen(key));
+    memcpy(entry->key, key, strlen(key));
+    entry->next = this->head;
+#ifndef S_LINKED
+    entry->prev = 0;
+#endif
+
+#ifndef S_LINKED
+    if(this->head)
+        this->head->prev = entry;
+#endif
+
+    if(!this->tail)
+        this->tail = entry;
+    this->size++;
+
+    this->head = entry; 
 }
 
 void map_push_back(struct map *this, char *key, void *data)
 {
+    struct map_node *entry = malloc(sizeof(struct map_node)); 
+
+    entry->data = data;
+    entry->key = malloc(sizeof(char) * strlen(key));
+    memcpy(entry->key, key, strlen(key));
+    entry->next = 0;
+#ifndef S_LINKED
+    entry->prev = this->tail;
+#endif
+
+    if(this->tail)
+        this->tail->next = entry;
+
+    if(!this->head)
+        this->head = entry;
+    this->size++;
+
+    this->tail = entry;
 }
 
-void map_pop_front(struct map *this, char *key, void *data)
+void *map_pop_front(struct map *this)
 {
+    if(!this->head)
+        return 0;
+
+    struct map_node *entry = this->head;
+    void *data = entry->data;
+
+    if(entry->next)
+    {
+        this->head = entry->next;
+#ifndef S_LINKED
+        this->head->prev = 0;
+#endif
+    }
+    else
+    {
+        this->head = 0;
+        this->tail = 0;
+    }
+    this->size--;
+
+    free(entry);
+    return data;
 }
 
-void map_pop_back(struct map *this, char *key, void *data)
+void *map_pop_back(struct map *this)
 {
+    if(!this->head)
+        return 0;
+
+    struct map_node *entry = this->tail;
+    void *data = entry->data;
+
+    if(entry->prev)
+    {
+        this->tail = entry->prev;
+#ifndef S_LINKED
+        this->tail->next = 0;
+#endif
+    }
+    else
+    {
+        this->head = 0;
+        this->tail = 0;
+    }
+    this->size--;
+
+    free(entry);
+    return data;
 }
 
 void *map_get(struct map *this, char *key)
 {
+    if(!this->head)
+        return 0;
+
+    struct map_node *entry;
+    entry = this->head;
+    while(entry->next && strncmp(entry->key, key, strlen(key)))
+    {
+        entry = entry->next;     
+    }
+
+    return entry->data;
 }
 
 void *map_at(struct map *this, int x)
 {
+    if(!this->head)
+        return 0;
+
+    struct map_node *entry;
+    //walk forward if x < size/2
+#ifndef S_LINKED
+    if(x < this->size/2)
+    {
+#endif
+        entry = this->head;
+        while(entry->next && --x >= 0)
+        {
+            entry = entry->next;     
+        }
+#ifndef S_LINKED
+    }
+    else
+    {
+        x = this->size - x;
+        x--;
+
+        entry = this->tail;
+        while(entry->prev && --x >= 0)
+        {
+            entry = entry->prev;
+        }
+    }
+#endif
+
+    return entry->data;
+
 }
 
 void *map_remove(struct map *this, char *key)
 {
+    if(!this->head)
+        return 0;
+
+    void *data;
+
+    struct map_node **entry;
+    entry = &this->head;
+    while((*entry)->next && strncmp((*entry)->key, key, strlen(key)))
+    {
+        entry = &(*entry)->next;
+    }
+    if((*entry)->next )
+    {
+        (*entry)->next->prev = (*entry)->prev;
+    }
+    else
+    {
+        this->tail = (*entry)->prev;
+    }
+    
+    data = (*entry)->data;
+
+    struct map_node *free_entry = *entry;
+
+    *entry = (*entry)->next;
+
+    free(free_entry);
+    this->size--;
+
+    if(!this->size)
+    {
+        this->head = 0;
+        this->tail = 0;
+    }
+    
+    return data;
+
 }
 
 void *map_remove_at(struct map *this, int x)
 {
+    if(!this->head)
+        return 0;
+
+    void *data;
+
+    struct map_node **entry;
+    //walk forward if x < size/2
+#ifndef S_LINKED
+    if(x < this->size/2)
+    {
+#endif
+        entry = &this->head;
+        while((*entry)->next && --x >= 0)
+        {
+            entry = &(*entry)->next;
+        }
+        if((*entry)->next )
+        {
+            (*entry)->next->prev = (*entry)->prev;
+        }
+        else
+        {
+            this->tail = (*entry)->prev;
+        }
+        
+        data = (*entry)->data;
+
+        struct map_node *free_entry = *entry;
+
+        *entry = (*entry)->next;
+
+        free(free_entry);
+#ifndef S_LINKED
+    }
+    else
+    {
+        x = this->size - x;
+        x--;
+
+        entry = &this->tail;
+        while((*entry)->prev && --x >= 0)
+        {
+            entry = &(*entry)->prev;
+        }
+        if((*entry)->prev)
+        {
+            (*entry)->prev->next = (*entry)->next;
+        }
+        else
+        {
+            this->head = (*entry)->next;
+        }
+
+        data = (*entry)->data;
+
+        struct map_node *free_entry = *entry;
+
+        *entry = (*entry)->prev;
+
+        free(free_entry);
+    }
+#endif
+    this->size--;
+
+    if(!this->size)
+    {
+        this->head = 0;
+        this->tail = 0;
+    }
+    
+    return data;
+
 }
 
 
 //------------------------- 
 
 #endif
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- src/container/test.c
+++ src/container/test.c
@@ -8,130 +8,298 @@
 
 describe(container, 
 {    
-    it("push_front", 
+    subdesc(list, 
     {
-        struct list mlist;
-        list_init(&mlist);
-        for(int i = 0; i < 10; i++)
+        it("push_front", 
         {
-            list_push_front(&mlist, i);
-            asserteq(mlist.size, i+1);
-            asserteq(list_at(&mlist,0) , i);
-        }        
-        asserteq(mlist.size, 10);
+            struct list mlist;
+            list_init(&mlist);
+            for(int i = 0; i < 10; i++)
+            {
+                list_push_front(&mlist, i);
+                asserteq(mlist.size, i+1);
+                asserteq(list_at(&mlist,0) , i);
+            }        
+            asserteq(mlist.size, 10);
+        });
+
+        it("push_back", 
+        {
+            struct list mlist;
+            list_init(&mlist);
+            for(int i = 0; i < 10; i++)
+            {
+                list_push_back(&mlist, i);
+                asserteq(mlist.size, i+1);
+                asserteq(list_at(&mlist,9) , i);
+            }        
+            asserteq(mlist.size, 10);
+        });
+
+        it("list_delete", 
+        {
+            struct list mlist;
+            list_init(&mlist);
+            for(int i = 0; i < 10; i++)
+            {
+                list_push_front(&mlist, i);
+                asserteq(mlist.size, i+1);
+                asserteq(list_at(&mlist,0) , i);
+            } 
+            list_delete(&mlist);
+            assert(!mlist.size);
+            assert(!mlist.head);
+            assert(!mlist.tail);
+        });
+
+        it("pop_back",
+        {
+            struct list mlist;
+            list_init(&mlist);
+            for(int i = 0; i < 10; i++)
+            {
+                list_push_back(&mlist, i);
+                asserteq(mlist.size, i+1);
+                asserteq(list_at(&mlist,9) , i);
+            }
+            for(int i = 9; i >= 0; i--)
+            {
+                asserteq(list_pop_back(&mlist), i);
+            }
+            assert(!mlist.size);
+            assert(!mlist.head);
+            assert(!mlist.tail);
+        });
+
+        it("pop_front",
+        {
+            struct list mlist;
+            list_init(&mlist);
+            for(int i = 0; i < 10; i++)
+            {
+                list_push_back(&mlist, i);
+                asserteq(mlist.size, i+1);
+                asserteq(list_at(&mlist,9) , i);
+            }
+            for(int i = 0; i < 10; i++)
+            {
+                asserteq(list_pop_front(&mlist), i);
+            }
+            assert(!mlist.size);
+            assert(!mlist.head);
+            assert(!mlist.tail);
+        });
+
+        it("at",
+        {
+            struct list mlist;
+            list_init(&mlist);
+            for(int i = 0; i < 10; i++)
+            {
+                list_push_back(&mlist, i);
+                asserteq(mlist.size, i+1);
+                asserteq(list_at(&mlist,9) , i);
+            }
+            for(int i = 0; i < 10; i++)
+            {
+                asserteq(list_at(&mlist, i), i);
+            }
+            asserteq(mlist.size, 10);
+            assert(mlist.head);
+            assert(mlist.tail);
+        });
+
+        it("remove_at",
+        {
+            struct list mlist;
+            list_init(&mlist);
+            for(int i = 0; i < 10; i++)
+            {
+                list_push_back(&mlist, i);
+                asserteq(mlist.size, i+1);
+                asserteq(list_at(&mlist,9) , i);
+            }
+            asserteq(list_remove_at(&mlist, 0), 0);
+            asserteq(list_remove_at(&mlist, 8), 9);
+            asserteq(list_remove_at(&mlist, 3), 4);
+            asserteq(list_remove_at(&mlist, 5), 7);
+            asserteq(list_remove_at(&mlist, 4), 6);
+            asserteq(list_remove_at(&mlist, 4), 8);
+            asserteq(list_remove_at(&mlist, 1), 2);
+            asserteq(list_remove_at(&mlist, 0), 1);
+            asserteq(list_remove_at(&mlist, 1), 5);
+            asserteq(list_remove_at(&mlist, 0), 3);
+
+            asserteq(mlist.size, 0);
+            assert(!mlist.head);
+            assert(!mlist.tail);
+        });
     });
 
-    it("push_back", 
+    subdesc(map, 
     {
-        struct list mlist;
-        list_init(&mlist);
-        for(int i = 0; i < 10; i++)
+        it("push_front", 
         {
-            list_push_back(&mlist, i);
-            asserteq(mlist.size, i+1);
-            asserteq(list_at(&mlist,9) , i);
-        }        
-        asserteq(mlist.size, 10);
+            struct map mmap;
+            map_init(&mmap);
+            char *key = malloc(5);
+            strcpy(key, "akey");
+            for(int i = 0; i < 10; i++)
+            {
+                map_push_front(&mmap, key, i);
+                asserteq(mmap.size, i+1);
+                asserteq(map_at(&mmap,0) , i);
+                (*key)++;
+            }        
+            asserteq(map_get(&mmap, "ckey"), 2);
+            asserteq(mmap.size, 10);
+            free(key);
+        });
+
+        it("push_back", 
+        {
+            struct map mmap;
+            map_init(&mmap);
+            char *key = malloc(5);
+            strcpy(key, "akey");
+            for(int i = 0; i < 10; i++)
+            {
+                map_push_back(&mmap,key, i);
+                asserteq(mmap.size, i+1);
+                asserteq(map_at(&mmap,9) , i);
+                (*key)++;
+            }        
+            asserteq(map_get(&mmap, "bkey"), 1);
+            asserteq(mmap.size, 10);
+            free(key);
+        });
+
+        it("map_delete", 
+        {
+            struct map mmap;
+            map_init(&mmap);
+            char *key = malloc(5);
+            strcpy(key, "akey");
+            for(int i = 0; i < 10; i++)
+            {
+                map_push_front(&mmap, key, i);
+                asserteq(mmap.size, i+1);
+                asserteq(map_at(&mmap,0) , i);
+                (*key)++;
+            } 
+            asserteq(map_get(&mmap, "dkey"), 3);
+            map_delete(&mmap);
+            assert(!mmap.size);
+            assert(!mmap.head);
+            assert(!mmap.tail);
+            free(key);
+        });
+
+        it("pop_back",
+        {
+            struct map mmap;
+            map_init(&mmap);
+            char *key = malloc(5);
+            strcpy(key, "akey");
+            for(int i = 0; i < 10; i++)
+            {
+                map_push_back(&mmap, key, i);
+                asserteq(mmap.size, i+1);
+                asserteq(map_at(&mmap,9) , i);
+                (*key)++;
+            }
+            for(int i = 9; i >= 0; i--)
+            {
+                asserteq(map_pop_back(&mmap), i);
+            }
+            asserteq(map_get(&mmap, "akey"), 0);
+            assert(!mmap.size);
+            assert(!mmap.head);
+            assert(!mmap.tail);
+            free(key);
+        });
+
+        it("pop_front",
+        {
+            struct map mmap;
+            map_init(&mmap);
+            char *key = malloc(5);
+            strcpy(key, "akey");
+            for(int i = 0; i < 10; i++)
+            {
+                map_push_back(&mmap, key, i);
+                asserteq(mmap.size, i+1);
+                asserteq(map_at(&mmap,9) , i);
+                (*key)++;
+            }
+            for(int i = 0; i < 10; i++)
+            {
+                asserteq(map_pop_front(&mmap), i);
+            }
+            //returns 0 pointer
+            asserteq(map_get(&mmap, "ekey"), 0);
+            assert(!mmap.size);
+            assert(!mmap.head);
+            assert(!mmap.tail);
+            free(key);
+        });
+
+        it("at",
+        {
+            struct map mmap;
+            map_init(&mmap);
+            char *key = malloc(5);
+            strcpy(key, "akey");
+            for(int i = 0; i < 10; i++)
+            {
+                map_push_back(&mmap, key, i);
+                asserteq(mmap.size, i+1);
+                asserteq(map_at(&mmap,9) , i);
+                (*key)++;
+            }
+            for(int i = 0; i < 10; i++)
+            {
+                asserteq(map_at(&mmap, i), i);
+            }
+            asserteq(map_get(&mmap, "fkey"), 5);
+            asserteq(mmap.size, 10);
+            assert(mmap.head);
+            assert(mmap.tail);
+            free(key);
+        });
+
+        it("remove_at",
+        {
+            struct map mmap;
+            map_init(&mmap);
+            char *key = malloc(5);
+            strcpy(key, "akey");
+            for(int i = 0; i < 10; i++)
+            {
+                map_push_back(&mmap, key, i);
+                asserteq(mmap.size, i+1);
+                asserteq(map_at(&mmap,9) , i);
+                (*key)++;
+            }
+            asserteq(map_get(&mmap, "gkey"), 6);
+            asserteq(map_remove_at(&mmap, 0), 0);
+            asserteq(map_remove_at(&mmap, 8), 9);
+            asserteq(map_remove_at(&mmap, 3), 4);
+            asserteq(map_remove_at(&mmap, 5), 7);
+            asserteq(map_remove_at(&mmap, 4), 6);
+            asserteq(map_remove_at(&mmap, 4), 8);
+            asserteq(map_remove_at(&mmap, 1), 2);
+            asserteq(map_remove_at(&mmap, 0), 1);
+            asserteq(map_remove_at(&mmap, 1), 5);
+            asserteq(map_remove_at(&mmap, 0), 3);
+
+            asserteq(mmap.size, 0);
+            assert(!mmap.head);
+            assert(!mmap.tail);
+            free(key);
+        });
     });
 
-    it("list_delete", 
-    {
-        struct list mlist;
-        list_init(&mlist);
-        for(int i = 0; i < 10; i++)
-        {
-            list_push_front(&mlist, i);
-            asserteq(mlist.size, i+1);
-            asserteq(list_at(&mlist,0) , i);
-        } 
-        list_delete(&mlist);
-        assert(!mlist.size);
-        assert(!mlist.head);
-        assert(!mlist.tail);
-    });
-
-    it("pop_back",
-    {
-        struct list mlist;
-        list_init(&mlist);
-        for(int i = 0; i < 10; i++)
-        {
-            list_push_back(&mlist, i);
-            asserteq(mlist.size, i+1);
-            asserteq(list_at(&mlist,9) , i);
-        }
-        for(int i = 9; i >= 0; i--)
-        {
-            asserteq(list_pop_back(&mlist), i);
-        }
-        assert(!mlist.size);
-        assert(!mlist.head);
-        assert(!mlist.tail);
-    });
-
-    it("pop_front",
-    {
-        struct list mlist;
-        list_init(&mlist);
-        for(int i = 0; i < 10; i++)
-        {
-            list_push_back(&mlist, i);
-            asserteq(mlist.size, i+1);
-            asserteq(list_at(&mlist,9) , i);
-        }
-        for(int i = 0; i < 10; i++)
-        {
-            asserteq(list_pop_front(&mlist), i);
-        }
-        assert(!mlist.size);
-        assert(!mlist.head);
-        assert(!mlist.tail);
-    });
-
-    it("at",
-    {
-        struct list mlist;
-        list_init(&mlist);
-        for(int i = 0; i < 10; i++)
-        {
-            list_push_back(&mlist, i);
-            asserteq(mlist.size, i+1);
-            asserteq(list_at(&mlist,9) , i);
-        }
-        for(int i = 0; i < 10; i++)
-        {
-            asserteq(list_at(&mlist, i), i);
-        }
-        asserteq(mlist.size, 10);
-        assert(mlist.head);
-        assert(mlist.tail);
-    });
-
-    it("remove_at",
-    {
-        struct list mlist;
-        list_init(&mlist);
-        for(int i = 0; i < 10; i++)
-        {
-            list_push_back(&mlist, i);
-            asserteq(mlist.size, i+1);
-            asserteq(list_at(&mlist,9) , i);
-        }
-        asserteq(list_remove_at(&mlist, 0), 0);
-        asserteq(list_remove_at(&mlist, 8), 9);
-        asserteq(list_remove_at(&mlist, 3), 4);
-        asserteq(list_remove_at(&mlist, 5), 7);
-        asserteq(list_remove_at(&mlist, 4), 6);
-        asserteq(list_remove_at(&mlist, 4), 8);
-        asserteq(list_remove_at(&mlist, 1), 2);
-        asserteq(list_remove_at(&mlist, 0), 1);
-        asserteq(list_remove_at(&mlist, 1), 5);
-        asserteq(list_remove_at(&mlist, 0), 3);
-
-        asserteq(mlist.size, 0);
-        assert(!mlist.head);
-        assert(!mlist.tail);
-    });
 
 });
 
