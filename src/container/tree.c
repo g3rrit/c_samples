bbdf17005f52abbe876b715459a18e8ad0c1911f
pearisgreen <grproessl@web.de>
2018-06-04 12:09:55 +0200

    update

--- src/container/tree.c
+++ src/container/tree.c
@@ -4,6 +4,7 @@
 struct tree_node
 {
     void *val;
+    int pos;
     struct tree_node *left;
     struct tree_node *right;
 };
@@ -14,10 +15,39 @@ struct tree
     int size;
 };
 
-int tree_init(struct tree *this);
+void tree_init(struct tree *this);
+
+int tree_add(struct tree *this, void *val, int pos);
+
+int tree_remove(struct tree *this, int pos);
+
+int tree_delete(struct tree *this);
 
 #endif
 
 #ifndef TREE_C
+
+struct tree_node *rot_left(
+
+void tree_init(struct tree *this)
+{
+    this->root = 0;
+    this->size = 0;
+}
+
+int tree_add(struct tree *this, void *val, int pos)
+{
+    struct tree_node **entry = &this->root;
+
+    
+}
+
+int tree_remove(struct tree *this, int pos)
+{
+}
+
+int tree_delete(struct tree *this)
+{
+}
 
 #endif
