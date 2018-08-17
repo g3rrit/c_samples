3f77b397e404a1d9b346f31848a6802dc52d0f1b
pearisgreen <grproessl@web.de>
2018-08-17 17:59:13 +0200

    update

--- src/error/error.h
+++ src/error/error.h
@@ -1,6 +1,7 @@
 #ifndef ERROR_H
 #define ERROR_H
 
+#include "stdlib.h"
 
 struct error_t 
 {
@@ -22,40 +23,74 @@ struct result_t
 typedef struct result_t* result;
 
 
-#define err(_msg, _err_h)           {                                                                   \\
-                                        struct result_t _res = malloc(sizeof(struct result_t));         \\
-                                        _res->is_error = 1;                                             \\
-                                        struct error_t _err = malloc(sizeof(struct error_t));           \\
-                                        _err->msg = _msg;                                               \\
-                                        _err->err_h = _err_h;                                           \\
-                                        _res->err = _err;                                               \\
-                                        _res->err->prev_err = 0;                                        \\
-                                        return _res;                                                    \\
+#define err(__msg, __err_h)                                                                               \
+                                    {                                                                   \
+                                        char *_msg = __msg;                                             \
+                                        void (*_err_h)() = __err_h;                                     \
+                                        struct result_t *_res = malloc(sizeof(struct result_t));        \
+                                        _res->is_error = 1;                                             \
+                                        struct error_t *_err = malloc(sizeof(struct error_t));          \
+                                        _err->msg = _msg;                                               \
+                                        _err->err_h = _err_h;                                           \
+                                        _res->err = _err;                                               \
+                                        _res->err->prev_err = 0;                                        \
+                                        return _res;                                                    \
                                     }
 
-#define ok(_ok)                     {                                                                   \\
-                                        struct result_t _res = malloc(sizeof(struct result_t));         \\
-                                        _res->is_error = 0;                                             \\
-                                        _res->ok = _ok;                                                 \\
-                                        return _res;                                                    \\
+#define ok(__ok)                                                                                         \
+                                    {                                                                   \
+                                        void *_ok = __ok;                                               \
+                                        struct result_t *_res = malloc(sizeof(struct result_t));        \
+                                        _res->is_error = 0;                                             \
+                                        _res->ok = _ok;                                                 \
+                                        return _res;                                                    \
                                     }
 
-#define try(_res, _msg, _err_h)     _res->ok;                                                           \\
-                                    if(_res->is_error)                                                  \\
-                                    {                                                                   \\
-                                        struct error_t _n_err = malloc(sizeof(struct error_t));         \\
-                                        _n_err->msg = _msg;                                             \\
-                                        _n_err->err_h = _err_h;                                         \\
-                                        _n_err->prev_err = _res->err;                                   \\
-                                        _res->err = _n_err;                                             \\
-                                        return _res;                                                    \\
-                                    }                                                                   \\
+#define try(__ok, __res, __msg, __err_h)                                                                \
+                                    {                                                                   \
+                                        void *_ok = __ok;                                               \
+                                        struct result_t *_res = __res;                                  \
+                                        char *_msg = __msg;                                             \
+                                        void (*_err_h)() = __err_h;                                     \
+                                        if(_res->is_error)                                              \
+                                        {                                                               \
+                                            struct error_t *_n_err = malloc(sizeof(struct error_t));    \
+                                            _n_err->msg = _msg;                                         \
+                                            _n_err->err_h = _err_h;                                     \
+                                            _n_err->prev_err = _res->err;                               \
+                                            _res->err = _n_err;                                         \
+                                            return _res;                                                \
+                                        }                                                               \
+                                        else                                                            \
+                                        {                                                               \
+                                            _ok = _res->ok;                                             \
+                                        }                                                               \
+                                    }
 
-#define catch(_res)                 _res->ok;                                                           \\
-                                    char *msg = 0;                                                      \\
-                                    void (*err_h)() = 0;                                                \\
-                                    if(_res->is_error)                                                  \\
-                                    for(msg = _res->err->msg, err_h = _res->err->err_h;
+#define catch(__ok, __res, _for_each)                                                                   \
+                                    {                                                                   \
+                                        struct result_t *_res = __res;                                  \
+                                        void *_ok = __ok;                                               \
+                                        if(_res->is_error)                                              \
+                                        {                                                               \
+                                            struct error_t *_err = _res->err;                           \
+                                            while(_err)                                                 \
+                                            {                                                           \
+                                                void (*err_h)() = _err->err_h;                          \
+                                                char *msg = _err->msg;                                  \
+                                                _for_each;                                              \
+                                                struct error_t *_f_err = _err;                          \
+                                                _err = _err->prev_err;                                  \
+                                                free(_f_err);                                           \
+                                            }                                                           \
+                                            free(_res);                                                 \
+                                        }                                                               \
+                                        else                                                            \
+                                        {                                                               \
+                                            _ok = _res->ok;                                             \
+                                        }                                                               \
+                                    }                                   
+                                            
 
 
                 
--- src/error/main.c
+++ src/error/main.c
@@ -1,13 +1,50 @@
 #include "error.h"
+#include "stdio.h"
 
-int foo() 
+void loo_err_h(int *res)
 {
+    *res = 1;
+}
 
-    return 0;
+result loo()
+{
+    err("loo error", &loo_err_h);
+    int res = 0;
+    ok(res);
+}
+
+void foo_err_h(int *res)
+{
+    *res = 2;
+}
+
+result foo() 
+{
+    int res = 0;
+    try(res, loo(), "loo error in foo", &foo_err_h);
+    ok(res);
+}
+
+void bar_err_h(int *res)
+{
+    *res = 3;
+}
+
+result bar() 
+{
+    int res = 0;
+    try(res, foo(), "foo error in bar", &bar_err_h);
+    ok(res);
 }
 
 int main()
 {
+    int res = 0;
+    catch(res, bar(), {
+        int res = 0;
+        err_h(&res);
+        printf("error msg: %s res: %i\n", msg, res);
+    });            
     return 0;
 }
 
