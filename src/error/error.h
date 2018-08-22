50159b461a1b1c657712239e942f42f4800d9ecc
pearisgreen <grproessl@web.de>
2018-08-22 16:55:23 +0200

    update

--- src/error/error.h
+++ src/error/error.h
@@ -1,12 +1,15 @@
 #ifndef ERROR_H
 #define ERROR_H
 
-#include "stdlib.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 struct error_t 
 {
     char *msg;
-    char *func;
+    uint32_t func;
+    uint32_t num;
+
     void *err_d;
     struct error_t *prev_err;
 };
@@ -26,7 +29,7 @@ typedef struct result_t* result;
 #pragma GCC diagnostic ignored "-Wint-to-void-pointer-cast"
 
 
-#define err(__msg, __err_d)                                                                             \
+#define err(__msg, _func, _num, __err_d)                                                                \
                                     {                                                                   \
                                         char *_msg = __msg;                                             \
                                         void *_err_d = (void*)__err_d;                                  \
@@ -34,6 +37,8 @@ typedef struct result_t* result;
                                         _res->is_error = 1;                                             \
                                         struct error_t *_err = malloc(sizeof(struct error_t));          \
                                         _err->msg = _msg;                                               \
+                                        _err->func = _func;                                             \
+                                        _err->num = _num;                                               \
                                         _err->err_d = _err_d;                                           \
                                         _res->err = _err;                                               \
                                         _res->err->prev_err = 0;                                        \
@@ -63,7 +68,7 @@ typedef struct result_t* result;
                                         return _res;                                                    \
                                     }
 
-#define try(_ok, __res, __msg, __err_d)                                                                 \
+#define try(_ok, __res, __msg, _func, _num, __err_d)                                              \
                                     {                                                                   \
                                         struct result_t *_res = __res;                                  \
                                         char *_msg = __msg;                                             \
@@ -72,6 +77,8 @@ typedef struct result_t* result;
                                         {                                                               \
                                             struct error_t *_n_err = malloc(sizeof(struct error_t));    \
                                             _n_err->msg = _msg;                                         \
+                                            _n_err->func = _func;                                       \
+                                            _n_err->num = _num;                                         \
                                             _n_err->err_d = _err_d;                                     \
                                             _n_err->prev_err = _res->err;                               \
                                             _res->err = _n_err;                                         \
@@ -88,9 +95,13 @@ typedef struct result_t* result;
                                     {                                                                   \
                                         struct result_t *_res = __res;                                  \
                                         if(_res->is_error)                                              \
+                                        {                                                               \
                                             return _res;                                                \
+                                        }                                                               \
                                         else                                                            \
+                                        {                                                               \
                                             free(_res);                                                 \
+                                        }                                                               \
                                     }
 
 #define catch(_ok, __res, _for_each)                                                                    \
@@ -101,8 +112,10 @@ typedef struct result_t* result;
                                             struct error_t *_err = _res->err;                           \
                                             while(_err)                                                 \
                                             {                                                           \
-                                                void *err = _err->err_d;                                \
-                                                char *msg = _err->msg;                                  \
+                                                void *err_d = _err->err_d;                              \
+                                                char *err_msg = _err->msg;                              \
+                                                uint32_t err_num = _err->num;                           \
+                                                uint32_t err_func = _err->func;                         \
                                                 _for_each;                                              \
                                                 struct error_t *_f_err = _err;                          \
                                                 _err = _err->prev_err;                                  \
